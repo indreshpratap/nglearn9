c2366e62b0aa4f287d7d8fe1fa0f5779f7e58f93
Indresh <ips.indresh@gmail.com>
2018-07-08 11:46:02 +0530

    Property and Event binding

--- src/app/examples/bindings.component.ts
+++ src/app/examples/bindings.component.ts
@@ -10,9 +10,26 @@ export class BindingsComponent  {
    step = 1;
    currentDate = new Date();
    userinfo:any;
+    flag = false;
+    msgclass='info';
 
    constructor(){
        this.userinfo = {name:'Indresh',address:'Noida'};
+    //    setTimeout(()=>{
+    //     this.msgclass = 'success';
+    //     // this.flag=true;
+    //    },5000);
+   }
+
+   sayHello(ev){
+         console.log(ev);
+       alert('Hi there');
+       this.flag = !this.flag;
+   }
+
+   boxEvent(){
+     
+       this.msgclass = 'success';
    }
 
 }
