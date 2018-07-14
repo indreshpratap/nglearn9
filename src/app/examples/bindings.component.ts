fca7a7a4271be7713e8e6af602fbe1235f39ebd6
Indresh <ips.indresh@gmail.com>
2018-07-14 11:44:42 +0530

    added NgClass

--- src/app/examples/bindings.component.ts
+++ src/app/examples/bindings.component.ts
@@ -4,32 +4,57 @@ import { Component, OnInit } from '@angular/core';
     selector: 'app-bindings',
     templateUrl: './bindings.component.html'
 })
-export class BindingsComponent  {
-   heading = 'Bindings Example';
-   description = "Binding example component";
-   step = 1;
-   currentDate = new Date();
-   userinfo:any;
+export class BindingsComponent {
+    heading = 'Bindings Example';
+    description = "Binding example component";
+    step = 1;
+    currentDate = new Date();
+    userinfo: any;
     flag = false;
-    msgclass='info';
+    msgclass = 'info';
+    type_cls='alert';
 
-   constructor(){
-       this.userinfo = {name:'Indresh',address:'Noida'};
-    //    setTimeout(()=>{
-    //     this.msgclass = 'success';
-    //     // this.flag=true;
-    //    },5000);
-   }
+    conditions = {
+        bold:false,
+        italic:false,
+        height:false,
+        color:false
+    };
 
-   sayHello(ev){
-         console.log(ev);
-       alert('Hi there');
-       this.flag = !this.flag;
-   }
+    classes = [];
 
-   boxEvent(){
-     
-       this.msgclass = 'success';
-   }
+    constructor() {
+        this.userinfo = { name: 'Indresh', address: 'Noida' };
+        //    setTimeout(()=>{
+        //     this.msgclass = 'success';
+        //     // this.flag=true;
+        //    },5000);
+
+        this.classes = ['alert','fixed-bottom','alert-info'];
+    }
+
+    sayHello(ev) {
+        console.log(ev);
+        alert('Hi there');
+        this.flag = !this.flag;
+    }
+
+    boxEvent() {
+
+        this.msgclass = 'success';
+    }
+
+    setType(txtObj) {
+        this.type_cls = txtObj.value;
+        this.classes[2]=txtObj.value;
+    }
+
+    applyCondition(key){
+        // statically you can access an object in array style like
+        // this.conditions['bold']= !this.conditions['bold'];
+
+        // passing the key from parameter
+        this.conditions[key]= !this.conditions[key];
+    }
 
 }
