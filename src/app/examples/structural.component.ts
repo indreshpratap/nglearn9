265c46a25fc57d5a223226c630d0f97c4eced112
Indresh <ips.indresh@gmail.com>
2018-07-22 14:27:40 +0530

    added ngfor

--- src/app/examples/structural.component.ts
+++ src/app/examples/structural.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-structural',
     templateUrl: './structural.component.html',
+    styles:['.del{text-decoration:line-through}']
     // styleUrls: ['./structural.component.css']
 })
 export class StructuralComponent{
@@ -10,6 +11,8 @@ export class StructuralComponent{
     editing = true;
 
     todos = [];
+    doneTodos =[];
+    objTodos = [];
 
     todoInputItem;
 
@@ -23,7 +26,26 @@ export class StructuralComponent{
 
     addItem(){
         this.todos.push(this.todoInputItem);
+        this.objTodos.push({checked:false,done:false,todo:this.todoInputItem});
         this.todoInputItem = '';
+        
 
+    }
+
+    done(indx,todo){
+        this.doneTodos.push(todo);
+        this.todos.splice(indx,1);
+    }
+    pending(indx,todo){
+        this.todos.push(todo);
+        this.doneTodos.splice(indx,1);
+    }
+
+    markDoneAllChecked(){
+        for(let ot of this.objTodos){
+            if(ot.checked){
+                ot.done= true;
+            }
+        }
     }
 }
